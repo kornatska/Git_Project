230291018f7e68d33f0977ce7844caeae5f06e56
Anastasiia <anastasiia.kornatska@gmail.com>
2022-03-26 13:11:41 -0700

    Updated RemoveSpaces file

--- src/day0/RemoveSpaces.java
+++ src/day0/RemoveSpaces.java
@@ -4,8 +4,7 @@ public class RemoveSpaces {
 
     public static void main(String[] args) {
         String str = "a b c";
+        System.out.println(str);
     }
-
-    //TODO: Work on this tomorrow
 
 }
